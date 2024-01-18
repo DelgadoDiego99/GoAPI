7b6e4bbc8d4940d5874b90391a0244f035b99848
Delgado <diego.f.delgado@accenture.com>
2024-01-17 22:52:17 -0500

    Creating router group for Todo struct

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"todo/api/todo"
 
 	"github.com/gin-gonic/gin"
 )
 
+func sayHi(c *gin.Context) {
+	fmt.Println("Hello, world")
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", todo.GetAllTodo)
+	router.GET("/", sayHi)
+	todoRoute := router.Group("/todo")
+	{
+		todoRoute.GET("/", todo.GetAllTodo)
+	}
 	router.Run("localhost:8080")
 }
